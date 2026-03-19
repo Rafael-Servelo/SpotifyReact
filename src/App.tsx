c30d28b8a98d2a06668214ef6a3505f87ab5ed13
Rafael <rafael.servelo@hotmail.com>
2026-03-19 19:38:06 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./Login.tsx";
 import Callback from "./Callback.tsx";
 import Header from "./Header.tsx";
@@ -7,22 +7,20 @@ import Footer from "./Footer.tsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/callback" element={<Callback />} />
-        <Route
-          path="/home"
-          element={
-            <div className="flex flex-col min-h-screen bg-zinc-950">
-              <Header />
-              <Body />
-              <Footer />
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/callback" element={<Callback />} />
+      <Route
+        path="/home"
+        element={
+          <div className="flex flex-col min-h-screen bg-zinc-950">
+            <Header />
+            <Body />
+            <Footer />
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
